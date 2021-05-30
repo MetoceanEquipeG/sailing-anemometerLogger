8ca4bfe72ce77a1e1948fc495e1a70a7fec030e0
pliniobas <pliniobas@gmail.com>
2021-05-30 11:13:11 +0000

    Antes de implementar metodo alternativo para metpy

--- sailing-anemometerLogger.py
+++ sailing-anemometerLogger.py
@@ -14,17 +14,51 @@ import re
 import sys
 import gc
 import datetime
-from metpy import calc
+# from metpy import calc
+from metpy.calc import wind_speed,wind_direction,wind_components
 from metpy.units import units
+import numpy as np
 
 gc.collect()
 
+import json
+
+try:
+    with open('sailing-anemometerLogger-config.txt') as f:
+        temp = f.read()
+        config = json.loads(temp)
+except Exception as e:
+    print(e)
+    print("Nao foi possivel abrir arquivo de configuração. Criando modelo novo.")
+    config = dict(
+                legSER = "comport e baudrate se referem a porta serial do anemometro",
+                comport = "COM30",
+                baudrate = 19200,
+                legTCP = "TCP_IP e TCP_PORT se referem ao endereco de IP do PC da navegacao e a porta aberta no Hypack Shared Memory",
+                TCP_IP = "127.0.0.1",
+                TCP_PORT = 8001,
+                )
+    temp = json.dumps(config,indent = 4)
+    with open('sailing-anemometerLogger-config.txt','w') as f:
+        f.write(temp)
+        f.close()
+
+
+comport = config["comport"]
+baudrate = config["baudrate"]
+TCP_IP = config["TCP_IP"]
+TCP_PORT = config["TCP_PORT"]
+
+if "ser" in globals() or "ser" in locals():
+    print("fechando a porta serial aberta previamente")
+    ser.close()
+
 #configuracao de portas
-comport = "COM30" #porta serial de recepcao da string do anemometro-emet, entre "aspas"
-baudrate = 19200 #baudrate do anemometro / emet
+# comport = "COM30" #porta serial de recepcao da string do anemometro-emet, entre "aspas"
+# baudrate = 19200 #baudrate do anemometro / emet
 # se o log for feito na mesma maquina da navegacao, o tcpIP pode ser 127.0.0.1
-TCP_IP = "127.0.0.1" #numero do IP da maquina da navegacao. entre "aspas"
-TCP_PORT = 8001 #numero da porta aberta no shared memory do hypack
+# TCP_IP = "127.0.0.1" #numero do IP da maquina da navegacao. entre "aspas"
+# TCP_PORT = 8001 #numero da porta aberta no shared memory do hypack
 
 #Exemplo da configuracao:
 #comport = "COM30" #porta serial de recepcao da string do anemometro-emet, entre "aspas"
@@ -32,13 +66,6 @@ TCP_PORT = 8001 #numero da porta aberta no shared memory do hypack
 # se o log for feito na mesma maquina da navegacao, o tcpIP pode ser 127.0.0.1
 #TCP_IP = "127.0.0.1" #numero do IP da maquina da navegacao. entre "aspas"
 #TCP_PORT = 8001 #numero da porta aberta no shared memory do hypack
-
-
-if "ser" in globals() or "ser" in locals():
-    print("fechando a porta serial aberta previamente")
-    ser.close()
-
-
 
 temp = """
 Esse programa ira gravar em um arquivo os dados medidos anemometro ja com a correcao 
@@ -74,6 +101,91 @@ print(temp)
 time.sleep(3)
 
 header = "wspdT,wdirT,wspd,wdir,boatspd,boatdir,lat,lon,gpstime\n"
+#%%
+def spdir2uv(spd, ang, deg=False):
+    """
+    Computes u, v components from speed and direction.
+    Parameters
+    ----------
+    spd : array_like
+          speed [m s :sup:`-1`]
+    ang : array_like
+          direction [deg]
+    deg : bool
+          option, True if data is in degrees. Default is False
+    Returns
+    -------
+    u : array_like
+        zonal wind velocity [m s :sup:`-1`]
+    v : array_like
+        meridional wind velocity [m s :sup:`-1`]
+    """
+
+    if deg:
+        ang = np.deg2rad(ang)
+
+    # Calculate U (E-W) and V (N-S) components
+    u = spd * np.sin(ang)
+    v = spd * np.cos(ang)
+    return u, v
+
+
+# In[4]:
+
+
+def uv2spdir(u, v, mag=0, rot=0):
+    """
+    Computes speed and direction from u, v components.
+    Converts rectangular to polar coordinate, geographic convention
+    Allows for rotation and magnetic declination correction.
+ 
+    Parameters
+    ----------
+    u : array_like
+        zonal wind velocity [m s :sup:`-1`]
+    v : array_like
+        meridional wind velocity [m s :sup:`-1`]
+    mag : float, array_like, optional
+          Magnetic correction [deg]
+    rot : float, array_like
+          Angle for rotation [deg]
+ 
+    Returns
+    -------
+    ang : array_like
+          direction [deg]
+    spd : array_like
+          speed [m s :sup:`-1`]
+ 
+    Notes
+    -----
+    Zero degrees is north.
+ 
+    Examples
+    --------
+    >>> import matplotlib.pyplot as plt
+    >>> from oceans.ocfis import uv2spdir
+    >>> u = [+0, -0.5, -0.50, +0.90]
+    >>> v = [+1, +0.5, -0.45, -0.85]
+    >>> wd, ws = uv2spdir(u,v)
+    >>> fig, ax = plt.subplots(subplot_kw=dict(polar=True))
+    >>> kw = dict(arrowstyle="->")
+    >>> wd = np.deg2rad(wd)
+    >>> lines = [ax.annotate("", xy=(d, s), xytext=(0, 0), arrowprops=kw)
+    ...  for d, s in zip(wd, ws)]
+    >>> _ = ax.set_ylim(0, np.max(ws))
+ 
+    """
+ 
+    u, v, mag, rot = list(map(np.asarray, (u, v, mag, rot)))
+ 
+    vec = u + 1j * v
+    spd = np.abs(vec)
+    ang = np.angle(vec, deg=True)
+    ang = ang - mag + rot
+    ang = np.mod(90. - ang, 360.)  # Zero is North.
+ 
+    return ang, spd
 
 
 
@@ -172,19 +284,19 @@ while True:
             pass
     
         #calculando a resultante        
-        wu,wv = calc.wind_components(wspd * units('knots'), wdir * units.degree)
+        wu,wv = wind_components(wspd * units('knots'), wdir * units.degree)
         
         #o vetor do barco e do vento tem direcoes contrarias, por causa da convecao de onde o vento vem,
         #mas o vetor do barco nao precisa ser girado pois essa funcao ja calcula o vetor invertido.
-        boatu,boatv = calc.wind_components(boatspd * units('knots'), boathdt * units.degree)
+        boatu,boatv = wind_components(boatspd * units('knots'), boathdt * units.degree)
         
         #velocidade medida = velocidade barco + velocidade real
         #velocidade real = velocidade medida - velocidade barco.
         wTu = wu - boatu
         wTv = wv - boatv
         
-        wspdT = float(calc.wind_speed(wTu,wTv).magnitude)
-        wdirT = float(calc.wind_direction(wTu,wTv).magnitude)
+        wspdT = float(wind_speed(wTu,wTv).magnitude)
+        wdirT = float(wind_direction(wTu,wTv).magnitude)
         
         lat = rmc.lat + ' ' + rmc.lat_dir
         lon = rmc.lon + ' ' + rmc.lon_dir        
